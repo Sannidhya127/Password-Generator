7858dccd8d8facf023e4b104bf0a3ff35d64e6b6
Sannidhya127 <shubhradasgupta8@gmail.com>
2021-04-18 17:14:41 +0530

    Made the password generator. Prints a random password each time. The program is done

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a random password generator. The passwords that this program generates are a bit tricky and hard to remember. Once I learn AI I will make this more user friendly
+
+
+import random
+import string
+
+alphabet_string = string.ascii_lowercase
+# Create a string of all lowercase letters
+print(alphabet_string)
+
+# alphabet_list = list(alphabet_string)
+
+numbers = "123456789"
+
+
+for i in range(3):
+    AlphaPassPt = random.choice(alphabet_string)
+    NumPassPt = random.choice(numbers)
+    print(f"{AlphaPassPt}{NumPassPt}", end='')
